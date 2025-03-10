6c010421348a66fdc8483be3bbe65aa37cc9bb0e
ScuBix1 <bastian.monnin1@gmail.com>
2025-03-10 15:17:08 +0100

    docs(API): add swagger in progress

--- README.md
+++ README.md
@@ -57,6 +57,13 @@ $ npm run test:e2e
 $ npm run test:cov
 ```
 
+## Generate SWAGGER
+
+```bash
+# command
+$ swagger-cli bundle -o swagger.yaml -t yaml swagger.json
+```
+
 ## Deployment
 
 When you're ready to deploy your NestJS application to production, there are some key steps you can take to ensure it runs as efficiently as possible. Check out the [deployment documentation](https://docs.nestjs.com/deployment) for more information.
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,35 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { writeFileSync } from 'fs';
+import * as yaml from 'js-yaml';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Math&Magique')
+    .setDescription("Description de l'API de Math&Magique")
+    .setVersion('1.0')
+    .addTag('Maths')
+    .build();
+
+  const documentFactory = SwaggerModule.createDocument(app, config);
+
+  console.log('📌 Swagger Document:', documentFactory);
+
+  if (documentFactory) {
+    writeFileSync('./swagger.json', JSON.stringify(documentFactory, null, 2));
+    writeFileSync('./swagger.yaml', yaml.dump(documentFactory));
+    console.log('✅ Swagger documentation générée en JSON et YAML');
+  } else {
+    console.error(
+      '❌ Erreur : `document` est undefined, vérifie la configuration Swagger.',
+    );
+  }
+
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(3000);
 }
 bootstrap();
